93d5dfcdaf148b4a31171b56eb78747e8cfbc4e7
Nhi111111 <mynhidn05@gmail.com>
2025-09-15 09:17:37 +0700

    Add files via upload

---
+++ 4.2.py
@@ -0,0 +1,20 @@
+def mahoa(text, k):
+    k = k % 26
+    ctext = ""
+    for ch in text:
+        if ch.isalpha():
+            if ch.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+            ctext += chr((ord(ch) - base + k) % 26 + base)
+        else:
+            ctext += ch
+    return ctext
+
+# chạy thử
+p = "P"
+k = 40
+c = mahoa(p, k)
+print("Ban ro:", p)
+print("Ban ma:", c)
